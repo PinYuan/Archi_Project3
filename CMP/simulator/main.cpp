982c064d4ff890c1c785e1f62df0e44c36d86be3
PinYuan <c0919156850@gmail.com>
2017-05-18 00:28:34 +0800

    add multi argus in command

--- CMP/simulator/main.cpp
+++ CMP/simulator/main.cpp
@@ -59,7 +59,71 @@ int main(int argc , char *argv[])
     totalDCacheSize = 16;
     DCacheBlockSize = 4;
     DCacheSetAssociativity = 1;
-    if(argc == 11){
+	
+	if(argc == 2){
+        IMemorySize = atoi(argv[1]);
+    }
+    else if(argc == 3){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+    }
+    else if(argc == 4){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+        IMemoryPageSize =  atoi(argv[3]);
+    }
+    else if(argc == 5){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+        IMemoryPageSize =  atoi(argv[3]);
+        DMemoryPageSize = atoi(argv[4]);
+    }
+    else if(argc == 6){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+        IMemoryPageSize =  atoi(argv[3]);
+        DMemoryPageSize = atoi(argv[4]);
+        totalICacheSize = atoi(argv[5]);
+    }
+    else if(argc == 7){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+        IMemoryPageSize =  atoi(argv[3]);
+        DMemoryPageSize = atoi(argv[4]);
+        totalICacheSize = atoi(argv[5]);
+        ICacheBlockSize = atoi(argv[6]);
+    }
+    else if(argc == 8){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+        IMemoryPageSize =  atoi(argv[3]);
+        DMemoryPageSize = atoi(argv[4]);
+        totalICacheSize = atoi(argv[5]);
+        ICacheBlockSize = atoi(argv[6]);
+        ICacheSetAssociativity = atoi(argv[7]);
+    }
+    else if(argc == 9){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+        IMemoryPageSize =  atoi(argv[3]);
+        DMemoryPageSize = atoi(argv[4]);
+        totalICacheSize = atoi(argv[5]);
+        ICacheBlockSize = atoi(argv[6]);
+        ICacheSetAssociativity = atoi(argv[7]);
+        totalDCacheSize = atoi(argv[8]);
+    }
+    else if(argc == 10){
+        IMemorySize = atoi(argv[1]);
+        DMemorySize = atoi(argv[2]);
+        IMemoryPageSize =  atoi(argv[3]);
+        DMemoryPageSize = atoi(argv[4]);
+        totalICacheSize = atoi(argv[5]);
+        ICacheBlockSize = atoi(argv[6]);
+        ICacheSetAssociativity = atoi(argv[7]);
+        totalDCacheSize = atoi(argv[8]);
+        DCacheBlockSize = atoi(argv[9]);
+    }
+    else if(argc == 11){
         IMemorySize = atoi(argv[1]);
         DMemorySize = atoi(argv[2]);
         IMemoryPageSize =  atoi(argv[3]);
